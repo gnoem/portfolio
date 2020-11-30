2619950c1aff0322ff9398b8739ed922dd42f410
Gnome <68836321+gnomelabs@users.noreply.github.com>
2020-11-29 22:38:18 -0700

    cleaned up

--- functions.js
+++ functions.js
@@ -2,7 +2,7 @@ window.onload = () => {
     window.addEventListener('scroll', () => {
         const bg = document.querySelector('#bg'),
             mid = document.querySelector('#mid');
-        var scrolled = window.pageYOffset, bgSpeed, midSpeed;
+        let scrolled = window.pageYOffset, bgSpeed, midSpeed;
         if (pageYOffset < window.innerWidth) {
             bgSpeed = (scrolled * (-0.2)) + 'px';
             midSpeed = (scrolled * (-0.65)) + 'px';
@@ -11,19 +11,19 @@ window.onload = () => {
         }
     });
     function fadeIn() {
-        var hiddenFade;
-        var hiddenSlide;
-        var windowHeight;
+        let hiddenFade;
+        let hiddenSlide;
+        let windowHeight;
         function init() {
             hiddenFade = document.querySelectorAll('.hidden-fade');
             hiddenSlide = document.querySelectorAll('.hidden-slide');
             windowHeight = window.innerHeight;
         }
         function checkPositionFade() {
-            for (var i = 0; i < hiddenFade.length; i++) {
-                var element = hiddenFade[i];
-                var bodyScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-                var positionFromTop = hiddenFade[i].getBoundingClientRect().top;
+            for (let i = 0; i < hiddenFade.length; i++) {
+                let element = hiddenFade[i];
+                let bodyScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+                let positionFromTop = hiddenFade[i].getBoundingClientRect().top;
                 if (positionFromTop - windowHeight <= 0) {
                     element.classList.add('fade-in-element');
                     element.classList.remove('hidden-fade');
@@ -35,10 +35,10 @@ window.onload = () => {
             }
         }
         function checkPositionSlide() {
-            for (var i = 0; i < hiddenSlide.length; i++) {
-                var element = hiddenSlide[i];
-                var bodyScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-                var positionFromTop = hiddenSlide[i].getBoundingClientRect().top;
+            for (let i = 0; i < hiddenSlide.length; i++) {
+                let element = hiddenSlide[i];
+                let bodyScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+                let positionFromTop = hiddenSlide[i].getBoundingClientRect().top;
                 if (positionFromTop - windowHeight <= 0) {
                     element.classList.add('slide-in-element');
                     element.classList.remove('hidden-slide');
